69bd28370df13ba8cb791a45a2346e2cf5fe9cc0
DevExpressExampleBot <fakeEmail>
2025-07-25 09:58:20 +0400

    Source auto update [skip ci]

--- VB/WpfApplication/ColorHelper/CellsHightlightHelper.vb
+++ VB/WpfApplication/ColorHelper/CellsHightlightHelper.vb
@@ -1,38 +1,20 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
-Imports System.Windows.Markup
 Imports System.Collections.ObjectModel
 Imports DevExpress.Xpf.Grid
 
 Namespace WpfApplication
-	Public NotInheritable Class CellsHightlightHelper
 
-		Public Shared ReadOnly CellsToHighlightProperty As DependencyProperty = DependencyProperty.RegisterAttached("CellsToHighlight", GetType(ObservableCollection(Of HighlightedGridCell)), GetType(CellsHightlightHelper), Nothing)
+    Public Module CellsHightlightHelper
 
+        Public ReadOnly CellsToHighlightProperty As DependencyProperty = DependencyProperty.RegisterAttached("CellsToHighlight", GetType(ObservableCollection(Of HighlightedGridCell)), GetType(CellsHightlightHelper), Nothing)
 
+        Public Function GetCellsToHighlight(ByVal target As DependencyObject) As ObservableCollection(Of HighlightedGridCell)
+            Return CType(target.GetValue(CellsToHighlightProperty), ObservableCollection(Of HighlightedGridCell))
+        End Function
 
-		Private Sub New()
-		End Sub
-		Public Shared Function GetCellsToHighlight(ByVal target As DependencyObject) As ObservableCollection(Of HighlightedGridCell)
-			Return CType(target.GetValue(CellsToHighlightProperty), ObservableCollection(Of HighlightedGridCell))
-		End Function
-		Public Shared Sub SetCellsToHighlight(ByVal target As GridControl, ByVal value As ObservableCollection(Of HighlightedGridCell))
-			target.SetValue(CellsToHighlightProperty, value)
-		End Sub
-
-
-
-	End Class
+        Public Sub SetCellsToHighlight(ByVal target As GridControl, ByVal value As ObservableCollection(Of HighlightedGridCell))
+            target.SetValue(CellsToHighlightProperty, value)
+        End Sub
+    End Module
 End Namespace
--- VB/WpfApplication/ColorHelper/HighlightedGridCell.vb
+++ VB/WpfApplication/ColorHelper/HighlightedGridCell.vb
@@ -1,70 +1,58 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
 Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
-Imports System.Windows.Markup
-Imports System.Collections.ObjectModel
 Imports DevExpress.Xpf.Grid
 
 Namespace WpfApplication
-	Public Class HighlightedGridCell
-		''' <summary>
-		''' Initializes a new instance of the HighlightedGridCell class.
-		''' </summary>
-		''' <param name="row"></param>
-		''' <param name="column"></param>
-		''' <param name="color"></param>
-		Public Sub New(ByVal row As Object, ByVal column As GridColumn, ByVal color As Color)
-			_Color = color
-			_Row = row
-			_Column = column
-		End Sub
-		' Fields...
-		Private _Color As Color
-		Private _Row As Object
-		Private _Column As GridColumn
 
-		Public Property Column() As GridColumn
-			Get
-				Return _Column
-			End Get
-			Set(ByVal value As GridColumn)
-				_Column = value
+    Public Class HighlightedGridCell
 
-			End Set
-		End Property
+        ''' <summary>
+        ''' Initializes a new instance of the HighlightedGridCell class.
+        ''' </summary>
+        ''' <param name="row"></param>
+        ''' <param name="column"></param>
+        ''' <param name="color"></param>
+        Public Sub New(ByVal row As Object, ByVal column As GridColumn, ByVal color As Color)
+            _Color = color
+            _Row = row
+            _Column = column
+        End Sub
 
+        ' Fields...
+        Private _Color As Color
 
-		Public Property Row() As Object
-			Get
-				Return _Row
-			End Get
-			Set(ByVal value As Object)
-				_Row = value
-			End Set
-		End Property
+        Private _Row As Object
 
+        Private _Column As GridColumn
 
+        Public Property Column As GridColumn
+            Get
+                Return _Column
+            End Get
 
-		Public Property Color() As Color
-			Get
-				Return _Color
-			End Get
-			Set(ByVal value As Color)
-				_Color = value
+            Set(ByVal value As GridColumn)
+                _Column = value
+            End Set
+        End Property
 
-			End Set
-		End Property
+        Public Property Row As Object
+            Get
+                Return _Row
+            End Get
 
-	End Class
+            Set(ByVal value As Object)
+                _Row = value
+            End Set
+        End Property
+
+        Public Property Color As Color
+            Get
+                Return _Color
+            End Get
+
+            Set(ByVal value As Color)
+                _Color = value
+            End Set
+        End Property
+    End Class
 End Namespace
